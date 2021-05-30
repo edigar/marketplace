fe790c82b1aeb846e6f762a4dc33bcd075cb3799
Edigar Herculano <edigarhdev@gmail.com>
2021-05-30 11:05:54 -0300

    style: Remove unnecessary code

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -15,7 +15,7 @@ class ExampleTest extends TestCase
         $this->get('/');
 
         $this->assertEquals(
-            $this->app->version(), $this->response->getContent()
+            'Marketplace project (pre-alpha)', $this->response->getContent()
         );
     }
 }
